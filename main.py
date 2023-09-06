896eba499b8916c9a1f19ca5d84332443bb62f0a
dzaky1201 <dzakyhdr@gmail.com>
2023-09-06 20:19:01 +0700

    add split function in main

--- main.py
+++ main.py
@@ -5,12 +5,5 @@ username = input('masukan username : ')
 password = input('masukan password : ')
 syntax = input('masukan konfig : ')
 
-config = [
-    'conf t',
-    'int e1/0',
-    'no sh',
-    'ip addr 10.10.10.1 255.255.255.252'
-]
-
 conf = Config(address, username, password)
-conf.start(syntax)
+conf.start(syntax.split(', '))
